f76c04619e17a478c533b8770a8e93ed3b0a4b62
INGLGGG <ing.lggg@gmail.com>
2020-01-04 22:48:25 -0600

    First Components and Ideas

---
+++ pomodoroclock/src/App.jsx
@@ -0,0 +1,22 @@
+import React , {Component} from 'react';
+import './App.css';
+
+
+export default class App extends Component {
+
+  //Se necesita un constructor para poder inicializar los estados
+  constructor(props){
+    super(props);
+
+
+    this.state = {
+      time: '',
+      isStart: false,
+    }
+  }
+
+
+
+
+
+}
---
+++ pomodoroclock/src/TimerSetter.jsx
@@ -0,0 +1,8 @@
+import React , {Component} from 'react';
+import './TimerSetter.css';
+
+export default class TimerSetter extends Component{
+
+}
+
+
